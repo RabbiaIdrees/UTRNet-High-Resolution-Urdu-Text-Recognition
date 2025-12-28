c993944801d4085ab0338958d25fea5b98a75cf6
RabbiaIdrees <rabbiaidrees70@gmail.com>
2025-12-28 23:03:33 +0500

    Change import of _accumulate to use itertools
    Replaced import of _accumulate from torch._utils with accumulate from itertools.

--- dataset.py
+++ dataset.py
@@ -18,7 +18,7 @@ import random
 import numpy as np
 from PIL import Image
 import torchvision.transforms as T
-from torch._utils import _accumulate
+!sed -i "s/from torch._utils import _accumulate/from itertools import accumulate as _accumulate/" dataset.py
 from torch.utils.data import Dataset, ConcatDataset, Subset
 
 class Batch_Balanced_Dataset(object):
